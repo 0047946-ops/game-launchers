bffa928b4b17ef4704989788efb0e9e2f269c123
0047946-ops <0047946@gmail.com>
2026-07-27 20:07:15 +0800

    Update save_hook.js

--- save_hook.js
+++ save_hook.js
@@ -1,1 +1,7 @@
-
+{
+  "version": "1.0.1",
+  "build": 101,
+  "changelog": "修復存檔雙向匯出/匯入邏輯，優化外掛載入速度",
+  "save_hook_url": "https://raw.githubusercontent.com/0047946-ops/game-launchers/main/save_hook.js",
+  "main_script_url": "https://raw.githubusercontent.com/0047946-ops/game-launchers/main/scripts/main.user.js"
+}
