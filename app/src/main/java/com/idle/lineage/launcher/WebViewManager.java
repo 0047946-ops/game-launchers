b25ec36ed878b81625b6ad6cf377b16825adecaa
0047946-ops <0047946@gmail.com>
2026-07-31 20:42:28 +0800

    Create WebViewManager.java

---
+++ app/src/main/java/com/idle/lineage/launcher/WebViewManager.java
@@ -0,0 +1,30 @@
+package com.idle.lineage.launcher;
+
+import android.content.Context;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class WebViewManager {
+    public static WebView createConfiguredWebView(Context context) {
+        WebView webView = new WebView(context);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
+
+        return webView;
+    }
+}
