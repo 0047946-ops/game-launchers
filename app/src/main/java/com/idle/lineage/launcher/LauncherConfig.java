661fd4e2e9e1d8d2bfe2650662f20d342432c624
0047946-ops <0047946@gmail.com>
2026-07-31 20:43:37 +0800

    Create LauncherConfig.java

---
+++ app/src/main/java/com/idle/lineage/launcher/LauncherConfig.java
@@ -0,0 +1,8 @@
+package com.idle.lineage.launcher;
+
+public class LauncherConfig {
+    public static final String SERVER_ONE = "https://pp771007.github.io/idle-lineage-class/";
+    public static final String SERVER_TWO = "https://shines871.github.io/idle-lineage-class/";
+    public static final String DEFAULT_LAUNCHER_URL = "https://0074946.github.io/launcher/";
+    public static final int FILE_CHOOSER_RESULT_CODE = 10001;
+}
