3e87d1382ea11b7e16185d374696ed178a14b973
0047946-ops <0047946@gmail.com>
2026-08-01 04:52:30 +0800

    Update WebViewManager.java

--- app/src/main/java/com/idle/lineage/launcher/WebViewManager.java
+++ app/src/main/java/com/idle/lineage/launcher/WebViewManager.java
@@ -4,78 +4,196 @@ import android.content.Context;
 import android.os.Build;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
+import android.webkit.WebStorage;
 import android.webkit.WebView;
-import android.webkit.WebViewDatabase;
+
+
 
 public class WebViewManager {
 
-    public static WebView createConfiguredWebView(Context context) {
 
-        WebView webView = new WebView(context);
+    public static WebView createConfiguredWebView(
+            Context context
+    ){
 
-        WebSettings settings = webView.getSettings();
+
+        WebView webView =
+                new WebView(context);
+
+
+
+        WebSettings settings =
+                webView.getSettings();
+
+
+
 
         // JavaScript
+
         settings.setJavaScriptEnabled(true);
+
         settings.setJavaScriptCanOpenWindowsAutomatically(true);
 
-        // HTML5
+
+
+
+        // HTML5 儲存
+
         settings.setDomStorageEnabled(true);
+
         settings.setDatabaseEnabled(true);
 
-        // 檔案
+
+
+
+        // Cache
+
+        settings.setCacheMode(
+                WebSettings.LOAD_DEFAULT
+        );
+
+
+
+
+        // 檔案權限
+
         settings.setAllowFileAccess(true);
+
         settings.setAllowContentAccess(true);
+
         settings.setAllowFileAccessFromFileURLs(true);
+
         settings.setAllowUniversalAccessFromFileURLs(true);
 
-        // 快取
-        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+
 
         // 混合內容
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        if(Build.VERSION.SDK_INT
+                >=
+           Build.VERSION_CODES.LOLLIPOP){
+
+
+            settings.setMixedContentMode(
+                    WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            );
+
+
         }
 
-        // 圖片
+
+
+
+        // 顯示
+
         settings.setLoadsImagesAutomatically(true);
 
-        // Viewport
         settings.setUseWideViewPort(true);
+
         settings.setLoadWithOverviewMode(true);
 
-        // 縮放
-        settings.setSupportZoom(false);
-        settings.setBuiltInZoomControls(false);
-        settings.setDisplayZoomControls(false);
 
-        // Media
+
+
+        // 遊戲音效
+
         settings.setMediaPlaybackRequiresUserGesture(false);
 
-        // User-Agent
-        String ua = settings.getUserAgentString();
-        ua = ua.replace("; wv", "");
-        ua = ua.replace("Version/4.0 ", "");
+
+
+
+        // User Agent 偽裝 Chrome
+
+        String ua =
+                settings.getUserAgentString();
+
+
+        ua =
+                ua.replace(
+                        "; wv",
+                        ""
+                );
+
+
+        ua =
+                ua.replace(
+                        "Version/4.0 ",
+                        ""
+                );
+
+
         settings.setUserAgentString(ua);
 
+
+
+
+
+
         // Cookie
-        CookieManager cookie = CookieManager.getInstance();
+
+        CookieManager cookie =
+                CookieManager.getInstance();
+
+
+
         cookie.setAcceptCookie(true);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            cookie.setAcceptThirdPartyCookies(webView, true);
+
+
+        if(Build.VERSION.SDK_INT
+                >=
+           Build.VERSION_CODES.LOLLIPOP){
+
+
+            cookie.setAcceptThirdPartyCookies(
+                    webView,
+                    true
+            );
+
+
         }
+
+
 
         cookie.flush();
 
+
+
+
+
+
+        // WebStorage 初始化
+
+        WebStorage
+                .getInstance();
+
+
+
+
+
+
         // 硬體加速
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null);
+
+        if(Build.VERSION.SDK_INT
+                >=
+           Build.VERSION_CODES.KITKAT){
+
+
+            webView.setLayerType(
+                    WebView.LAYER_TYPE_HARDWARE,
+                    null
+            );
+
+
         }
 
-        // 初始化 WebView 資料庫
-        WebViewDatabase.getInstance(context);
+
 
         return webView;
+
+
     }
+
+
 }
